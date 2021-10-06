bc3ee3a21e0b59fa82c7677655305b532bbf6d43
Leo Yu-Chi Liang <ycliang@andestech.com>
2021-10-06 11:18:05 +0800

    Fix reboot feature
    The corresponding Linux commit is
    ec7e3bd0b86b31d423ed305a6f137b288bdc1bf2
    
    Reboot uses SMU reset command on SMUCR(Command Register).
    This reset command acts like a reboot button,
    it does not care which hart press it.
    Reboot in a violent manner regardless of the system status.
    (i.e. I/Dcache, L2, other harts status)
    
    Signed-off-by: Leo Yu-Chi Liang <ycliang@andestech.com>
    Reviewed-by: Dylan Jhong <dylan@andestech.com>
    Reviewed-by: Rick Chen <rick@andestech.com>

--- platform/andes/ae350/smu.h
+++ platform/andes/ae350/smu.h
@@ -25,10 +25,12 @@
 
 #define DRAM_BASE		0x80000000
 #define SMU_BASE		0xf0100000
+#define SMUCR_OFF		0x14
+#define SMUCR_RESET		0x3c
 #define PCS0_CTL_OFF			0x94
 #define SMU_RESET_VEC_OFF		0x50
 #define SMU_RESET_VEC_PER_CORE  0x4
-#define RESET_CMD               0x1
+#define PCS_RESET           0x1
 
 #define MAX_PCS_SLOT    7
 
