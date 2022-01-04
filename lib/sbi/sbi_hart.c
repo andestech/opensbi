79eadf38c5e41a3ebe006eba317bd392cbaa666d
Leo Yu-Chi Liang <ycliang@andestech.com>
2022-01-04 14:33:22 +0800

    Add fence.i as a workaround for bugzilla 24304
    bugzilla entry (http://e-andes.andestech.com/bugzilla5/show_bug.cgi?id=24304)
    
    Add fence.i as a workaroud.
    
    Signed-off-by: Leo Yu-Chi Liang <ycliang@andestech.com>
    Reviewed-by: Alan Kao <alankao@andestech.com>

--- lib/sbi/sbi_hart.c
+++ lib/sbi/sbi_hart.c
@@ -497,6 +497,7 @@ sbi_hart_switch_mode(unsigned long arg0, unsigned long arg1,
 		csr_write(CSR_UIE, 0);
 	}
 
+	asm("fence.i");
 	register unsigned long a0 asm("a0") = arg0;
 	register unsigned long a1 asm("a1") = arg1;
 	__asm__ __volatile__("mret" : : "r"(a0), "r"(a1));
