4aa323f3634b9ef000438fb75ab2421bbf5ca7ca
Dylan Jhong <dylan@andestech.com>
2022-05-28 03:23:24 +0800

    ae350/config.mk: Disable jump/payload mode
    Our releases only need to support one of the OpenSBI mode,
    so turn off jump/payload mode and leave only dynamic mode
    which is used with U-Boot SPL to turn on the caches.
    
    Signed-off-by: Dylan Jhong <dylan@andestech.com>
    Reviewed-by: Yu Chien Peter Lin <peterlin@andestech.com>

--- platform/generic/objects.mk
+++ platform/generic/objects.mk
@@ -24,7 +24,7 @@ platform-objs-y += platform_override_modules.o
 # Blobs to build
 FW_TEXT_START=0x00000000
 FW_DYNAMIC=y
-FW_JUMP=y
+FW_JUMP=n
 ifeq ($(PLATFORM_RISCV_XLEN), 32)
   # This needs to be 4MB aligned for 32-bit system
   FW_JUMP_ADDR=$(shell printf "0x%X" $$(($(FW_TEXT_START) + 0x400000)))
@@ -33,7 +33,7 @@ else
   FW_JUMP_ADDR=$(shell printf "0x%X" $$(($(FW_TEXT_START) + 0x200000)))
 endif
 FW_JUMP_FDT_ADDR=$(shell printf "0x%X" $$(($(FW_TEXT_START) + 0x2200000)))
-FW_PAYLOAD=y
+FW_PAYLOAD=n
 ifeq ($(PLATFORM_RISCV_XLEN), 32)
   # This needs to be 4MB aligned for 32-bit system
   FW_PAYLOAD_OFFSET=0x400000
