9b0987351405ffc34286928eb38ca15de23df842
Yu Chien Peter Lin <peterlin@andestech.com>
2023-01-19 15:42:44 +0800

    platform: generic: andes: support Andes platform
    Add Andes defconfig and build configuration
    
    Reformed from the following patches on ast-v5_3_0-branch
    - (7d4cd15d32) platform: generic: objects.mk: Adjust firmware text start address to 0x0
    - (4aa323f363) ae350/config.mk: Disable jump/payload mode
    - (993a82a729) platform: generic: defconfig: Disable unused platforms and drivers
    
    Signed-off-by: Hui Min Mina Chou <minachou@andestech.com>

--- platform/generic/andes/objects.mk
+++ platform/generic/andes/objects.mk
@@ -2,6 +2,15 @@
 # SPDX-License-Identifier: BSD-2-Clause
 #
 
+# Use Andes custom configuration
+PLATFORM_DEFCONFIG=andes_defconfig
+
+# Blobs to build
+FW_TEXT_START=0x00000000
+FW_JUMP=n
+FW_PAYLOAD=n
+
+# Objects to build
 carray-platform_override_modules-$(CONFIG_PLATFORM_ANDES_AE350) += andes_ae350
 platform-objs-$(CONFIG_PLATFORM_ANDES_AE350) += andes/ae350.o andes/sleep.o
 
