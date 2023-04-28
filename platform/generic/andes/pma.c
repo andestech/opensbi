andestech/opensbi

commit ed478347793e1ff305c21f25bef2341748f21546

platform: generic: andes: Add PMA SBI call
This patch provides mcall_{set,free,probe}_pma SBI calls to support
programmable PMA for the platforms without IOCP.

Signed-off-by: Yu Chien Peter Lin <peterlin@andestech.com>
Reviewed-by: Leo Yu-Chi Liang <ycliang@andestech.com>

platform/generic/andes/pma.c
@@ -1,0 +1,351 @@
+/*
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2020 Andes Technology Corporation
+ *
+ * Authors:
+ *	Nick Hu <nickhu@andestech.com>
+ *	Nylon Chen <nylon7@andestech.com>
+ *	Yu Chien Peter Lin <peterlin@andestech.com>
+ */
+
+#include <sbi/riscv_asm.h>
+#include <sbi/riscv_io.h>
+#include <sbi/sbi_bitops.h>
+#include <sbi/sbi_console.h>
+#include <sbi/sbi_error.h>
+#include <sbi/sbi_types.h>
+#include <andes/andes45.h>
+#include <andes/pma.h>
+
+/*
+ * pma_user_table[] keeps track of which PMA entry is being used,
+ * 0 indicates that it is unused and can be allocated.
+ */
+unsigned long pma_user_table[PMA_ENTRY_NR] = { 0 };
+
+static void andes_pma_write_num(int csr_num, unsigned long val)
+{
+#define switchcase_csr_write(__csr_num, __val) \
+	case __csr_num:                        \
+		csr_write(__csr_num, __val);   \
+		break;
+#define switchcase_csr_write_2(__csr_num, __val)   \
+	switchcase_csr_write(__csr_num + 0, __val) \
+		switchcase_csr_write(__csr_num + 1, __val)
+#define switchcase_csr_write_4(__csr_num, __val)     \
+	switchcase_csr_write_2(__csr_num + 0, __val) \
+		switchcase_csr_write_2(__csr_num + 2, __val)
+#define switchcase_csr_write_8(__csr_num, __val)     \
+	switchcase_csr_write_4(__csr_num + 0, __val) \
+		switchcase_csr_write_4(__csr_num + 4, __val)
+#define switchcase_csr_write_16(__csr_num, __val)    \
+	switchcase_csr_write_8(__csr_num + 0, __val) \
+		switchcase_csr_write_8(__csr_num + 8, __val)
+
+	switch (csr_num) {
+	switchcase_csr_write_4(CSR_PMACFG0, val)
+	switchcase_csr_write_16(CSR_PMAADDR0, val)
+	default:
+		sbi_panic("%s: Unknown Andes CSR %#x", __func__, csr_num);
+	break;
+	};
+
+#undef switchcase_csr_write_16
+#undef switchcase_csr_write_8
+#undef switchcase_csr_write_4
+#undef switchcase_csr_write_2
+#undef switchcase_csr_write
+}
+
+static unsigned long andes_pma_read_num(int csr_num)
+{
+#define switchcase_csr_read(__csr_num, __val) \
+	case __csr_num:                       \
+		__val = csr_read(__csr_num);  \
+		break;
+#define switchcase_csr_read_2(__csr_num, __val)   \
+	switchcase_csr_read(__csr_num + 0, __val) \
+		switchcase_csr_read(__csr_num + 1, __val)
+#define switchcase_csr_read_4(__csr_num, __val)     \
+	switchcase_csr_read_2(__csr_num + 0, __val) \
+		switchcase_csr_read_2(__csr_num + 2, __val)
+#define switchcase_csr_read_8(__csr_num, __val)     \
+	switchcase_csr_read_4(__csr_num + 0, __val) \
+		switchcase_csr_read_4(__csr_num + 4, __val)
+#define switchcase_csr_read_16(__csr_num, __val)    \
+	switchcase_csr_read_8(__csr_num + 0, __val) \
+		switchcase_csr_read_8(__csr_num + 8, __val)
+#define switchcase_csr_read_32(__csr_num, __val)     \
+	switchcase_csr_read_16(__csr_num + 0, __val) \
+		switchcase_csr_read_16(__csr_num + 16, __val)
+
+	unsigned long ret = 0;
+
+	switch (csr_num) {
+	switchcase_csr_read_4(CSR_PMACFG0, ret)
+	switchcase_csr_read_16(CSR_PMAADDR0, ret)
+	default:
+		sbi_panic("%s: Unknown Andes CSR %#x", __func__, csr_num);
+	break;
+	};
+
+	return ret;
+
+#undef switchcase_csr_read_16
+#undef switchcase_csr_read_8
+#undef switchcase_csr_read_4
+#undef switchcase_csr_read_2
+#undef switchcase_csr_read
+}
+
+static unsigned long read_pmaaddrx(int entry_id)
+{
+	return andes_pma_read_num(CSR_PMAADDR0 + entry_id);
+}
+
+static char read_pmaxcfg(int entry_id)
+{
+	unsigned long pmacfg_val;
+	unsigned long pmaxcfg_mask;
+
+	if (entry_id < 0 || entry_id > 15) {
+		sbi_panic("ERROR %s(): non-existing PMA entry_id: %d\n",
+			  __func__, entry_id);
+		return 0;
+	}
+
+
+#if __riscv_xlen == 64
+	pmacfg_val = andes_pma_read_num(CSR_PMACFG0 + ((entry_id / 8) ? 2 : 0));
+	pmaxcfg_mask = 0xfful << (8 * (entry_id % 8));
+#else
+	pmacfg_val   = andes_pma_read_num(CSR_PMACFG0 + (entry_id / 4));
+	pmaxcfg_mask = 0xfful << (8 * (entry_id % 4));
+#endif
+
+	return EXTRACT_FIELD(pmacfg_val, pmaxcfg_mask);
+}
+
+static bool is_va_alias(unsigned long va)
+{
+	/**
+	 * Check if the va conflicts with the existing one in
+	 * pma_user_table[]
+	 */
+	for (int i = 0; i < PMA_ENTRY_NR; i++) {
+		if (pma_user_table[i] != va)
+			continue;
+		sbi_printf("ERROR %s(): va %#lx conflicts\n", __func__,
+			  va);
+		return true;
+	}
+	return false;
+}
+
+static u8 pmaxcfg_etyp(int entry_id)
+{
+	/* pmaxcfg.ETYP will be 0 when it is set to 1 or 2 (reserved) */
+	u8 pmaxcfg_val = read_pmaxcfg(entry_id);
+	return EXTRACT_FIELD(pmaxcfg_val, PMACFG_ETPY_MASK);
+}
+
+static int allocate_pma_entry(unsigned long va, int *entry_id)
+{
+	/* Not allow va alias */
+	if(is_va_alias(va))
+		goto fail;
+
+	for (int i = 0; i < PMA_ENTRY_NR; i++) {
+		if (pmaxcfg_etyp(i) != PMACFG_ETYP_OFF)
+			continue;
+		pma_user_table[i] = va;
+		*entry_id	  = i;
+		return SBI_SUCCESS;
+	}
+
+fail:
+	sbi_printf("ERROR %s(): Cannot allocate PMA entry for %#lx\n", __func__,
+		   va);
+	*entry_id = -1;
+
+	return SBI_ENOENT;
+}
+
+static inline bool is_napot(unsigned long base, unsigned long size)
+{
+	return !((size & (size - 1)) || base & (size - 1));
+}
+
+bool mcall_probe_pma(void)
+{
+	return !!EXTRACT_FIELD(csr_read(CSR_MMSC_CFG), CSR_MMSC_CFG_PPMA_MASK);
+}
+
+static int pmaaddrx_to_region(int entry_id, unsigned long *out_start,
+			      unsigned long *out_size)
+{
+	/*
+	 * Given entry id, calculate the region start and size
+	 * @entry_id the PMA entry id (only 0 ~ 15)
+	 *
+	 * @out_start output value for start address
+	 * @out_size output value for region size
+	 *
+	 * Return 0 on success
+	 * Return SBI_EINVAL if PMA is not in NAPOT mode
+	 */
+	unsigned long pmaaddrx_val;
+	int k;
+
+	if (pmaxcfg_etyp(entry_id) == PMACFG_ETYP_NAPOT) {
+		pmaaddrx_val = read_pmaaddrx(entry_id);
+		/**
+		* Given $pmaaddr, let k = # of trailing 1s
+		* size = 2^(k + 3)
+		* base = 4 * ($pmaaddr - (size / 8) + 1)
+		*/
+		k     = sbi_ffz(pmaaddrx_val);
+		*out_size  = 1 << (k + 3);
+		*out_start = (pmaaddrx_val - (*out_size >> 3) + 1) << 2;
+		return 0;
+	}
+
+	sbi_printf("ERROR %s(): PMA%d is not NAPOT mode (enable)\n", __func__,
+		   entry_id);
+	*out_size = *out_start = -1;
+	return SBI_EINVAL;
+}
+
+static bool has_pma_region_conflict(unsigned long start, unsigned long size, int *conflict_id)
+{
+	unsigned long _start, _size, _end, end;
+
+	end = start + size - 1;
+
+	for (int i = 0; i < PMA_ENTRY_NR; i++) {
+		if (pmaxcfg_etyp(i) == PMACFG_ETYP_OFF)
+			continue;
+
+		/* Decode start address and its size from pmaaddrx */
+		if (pmaaddrx_to_region(i, &_start, &_size))
+			return true;
+
+		_end = _start + _size - 1;
+
+		if (max(start, _start) <= min(end, _end)) {
+			sbi_printf("ERROR %s(): desired region %#lx - %#lx (size: %#lx) conflicts with the existing PMA%d: %#lx - %#lx (size: %#lx)\n",
+					__func__, start, end, size, i, _start, _end, _size);
+			*conflict_id = i;
+			return true;
+		}
+	}
+
+	return false;
+}
+
+int mcall_set_pma(unsigned long pa, unsigned long va, unsigned long size)
+{
+	char *pmaxcfg;
+	int rc, pma_cfg, conflict_id = -1, entry_id;
+	unsigned long pmaaddr_val, pmacfg_val;
+
+	/* Sanity check */
+	if (!mcall_probe_pma()) {
+		sbi_printf("ERROR %s(): platform does not support PPMA.\n",
+			  __func__);
+		goto fail;
+	}
+
+	if (size < 4096 || !is_napot(pa, size)) {
+		sbi_printf("ERROR %s(): %#lx - %#lx (size: %#lx) is not a 4KiB granularity NAPOT region\n",
+			  __func__, pa, pa + size - 1, size);
+		goto fail;
+	}
+
+	/* Not allow region overlapping */
+	if (has_pma_region_conflict(pa, size, &conflict_id)) {
+		sbi_printf(
+			"ERROR %s(): PMA region overlaps with PMA%d used by va=%#lx\n",
+			__func__, conflict_id,
+			pma_user_table[conflict_id]);
+		goto fail;
+	}
+
+	/* Encode the start address and size */
+	pmaaddr_val = (pa >> 2) + (size >> 3) - 1;
+
+	rc = allocate_pma_entry(va, &entry_id);
+	if (rc)
+		return SBI_ENOENT;
+
+#if __riscv_xlen == 64
+	pma_cfg	   = CSR_PMACFG0 + ((entry_id / 8) ? 2 : 0);
+	pmacfg_val = andes_pma_read_num(pma_cfg);
+	pmaxcfg	   = (char *)&pmacfg_val + (entry_id % 8);
+	*pmaxcfg   = 0;
+	*pmaxcfg |= PMACFG_ETYP_NAPOT;
+	*pmaxcfg |= PMACFG_MTYP_NOCACHE_BUFFER;
+#else
+	pma_cfg	     = CSR_PMACFG0 + entry_id / 4;
+	pmacfg_val   = andes_pma_read_num(pma_cfg);
+	pmaxcfg	     = (char *)&pmacfg_val + (entry_id % 4);
+	*pmaxcfg     = 0;
+	*pmaxcfg |= PMACFG_ETYP_NAPOT;
+	*pmaxcfg |= PMACFG_MTYP_NOCACHE_BUFFER;
+#endif
+
+	/* Set pmaxcfg and pmaaddrx */
+	andes_pma_write_num(pma_cfg, pmacfg_val);
+	andes_pma_write_num(CSR_PMAADDR0 + entry_id, pmaaddr_val);
+
+	if (andes_pma_read_num(CSR_PMAADDR0 + entry_id) != pmaaddr_val) {
+		sbi_printf(
+			"ERROR %s(): Failed to set the pmaaddr%d to desired value\n",
+			__func__, entry_id);
+		return SBI_EFAIL;
+	}
+
+	return 0;
+
+fail:
+	return SBI_EINVAL;
+}
+
+int mcall_free_pma(unsigned long va)
+{
+	unsigned long pmacfg_val;
+	int pma_cfg;
+	char *pmaxcfg;
+
+	for (int i = 0; i < PMA_ENTRY_NR; i++) {
+		if (pma_user_table[i] != va)
+			continue;
+
+		if (pmaxcfg_etyp(i) == PMACFG_ETYP_OFF)
+			sbi_panic(
+				"ERROR %s(): expecting PMA%d is enabled for %#lx\n",
+				__func__, i, va);
+
+		/* Free the entry */
+		pma_user_table[i] = 0x0;
+#if __riscv_xlen == 64
+		pma_cfg	   = CSR_PMACFG0 + ((i / 8) ? 2 : 0);
+		pmacfg_val = andes_pma_read_num(pma_cfg);
+		pmaxcfg	   = (char *)&pmacfg_val + (i % 8);
+		*pmaxcfg   = PMACFG_ETYP_OFF;
+#else
+		pma_cfg	   = CSR_PMACFG0 + i / 4;
+		pmacfg_val = andes_pma_read_num(pma_cfg);
+		pmaxcfg	   = (char *)&pmacfg_val + (i % 4);
+		*pmaxcfg   = PMACFG_ETYP_OFF;
+#endif
+		andes_pma_write_num(pma_cfg, pmacfg_val);
+		return 0;
+	}
+
+	sbi_printf("ERROR %s(): failed to free the PMA entry used by %#lx\n",
+		   __func__, va);
+
+	return SBI_ENOENT;
+}

platform/generic/include/andes/pma.h
@@ -1,0 +1,59 @@
+/*
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2023 Andes Technology Corporation
+ *
+ * Authors:
+ *	Nick Hu <nickhu@andestech.com>
+ *	Nylon Chen <nylon7@andestech.com>
+ *	Yu Chien Peter Lin <peterlin@andestech.com>
+ */
+
+#ifndef _ANDES_PMA_H
+#define _ANDES_PMA_H
+
+#define CSR_PMAADDR0 0xBD0
+#define CSR_PMACFG0 0xBC0
+
+#define PMA_ENTRY_NR 16
+#define PMACFG_ETPY_MASK 0x3
+#define PMACFG_MTPY_MASK (0xf << 2)
+#define PMACFG_ETYP_OFF 0x0
+#define PMACFG_ETYP_NAPOT 0x3
+#define PMACFG_MTYP_NOCACHE_BUFFER 0x3
+
+#ifndef __ASSEMBLER__
+
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+
+/**
+ * Check if hardware support PPMA
+ */
+bool mcall_probe_pma(void);
+
+/**
+ * Allocate a PMA entry for a "memory, non-cacheable, bufferable" NAPOT region
+ * @param pa start address of NAPOT region
+ * @param va virtual address to be stored in pma_user_table[i] which
+ *           indicates the PMA entry has been allocated
+ * @param size size of NAPOT region
+ * @param entry_id (deprecated)
+ *
+ * @return 0 on success
+ * @return SBI_ENOENT if run out of the available entries
+ * @return SBI_EFAIL otherwise
+ */
+int mcall_set_pma(unsigned long pa, unsigned long va, unsigned long size);
+
+/**
+ * Free a PMA entry
+ * @param va virtual address, the associated PMA entry of the va will be freed
+ *
+ * @return 0 on success
+ * @return SBI_ENOENT if entry not found
+ */
+int mcall_free_pma(unsigned long va);
+
+#endif /* __ASSEMBLER__ */
+#endif /* _ANDES_PMA_H */

platform/generic/Kconfig
@@ -54,4 +54,8 @@ config ANDES_TRIGGER
 	bool "Andes trigger support"
 	default n
 
+config ANDES_PMA
+	bool "Andes programmable PMA support"
+	default n
+
 endif

platform/generic/andes/ae350.c
@@ -19,6 +19,7 @@
 #include <sbi/sbi_init.h>
 #include <andes/ae350.h>
 #include <andes/andes45.h>
+#include <andes/pma.h>
 #include <andes/trigger.h>
 
 static struct smu_data smu = { 0 };
@@ -150,6 +151,17 @@ static int ae350_vendor_ext_provider(long extid, long funcid,
 	case SBI_EXT_ANDES_WRITE_POWERBRAKE:
 		csr_write(CSR_MPFT_CTL, regs->a0);
 		break;
+#ifdef CONFIG_ANDES_PMA
+	case SBI_EXT_ANDES_SET_PMA:
+		ret = mcall_set_pma(regs->a0, regs->a1, regs->a2);
+		break;
+	case SBI_EXT_ANDES_FREE_PMA:
+		ret = mcall_free_pma(regs->a0);
+		break;
+	case SBI_EXT_ANDES_PROBE_PMA:
+		*out_value = mcall_probe_pma();
+		break;
+#endif
 	default:
 		sbi_panic("%s(): funcid: %#lx is not supported\n", __func__, funcid);
 		break;

platform/generic/andes/objects.mk
@@ -5,3 +5,4 @@
 carray-platform_override_modules-$(CONFIG_PLATFORM_ANDES_AE350) += andes_ae350
 platform-objs-$(CONFIG_PLATFORM_ANDES_AE350) += andes/ae350.o andes/sleep.o
 platform-objs-$(CONFIG_ANDES_TRIGGER) += andes/trigger.o
+platform-objs-$(CONFIG_ANDES_PMA) += andes/pma.o

platform/generic/configs/defconfig
@@ -10,3 +10,4 @@ CONFIG_FDT_SERIAL_UART8250=y
 CONFIG_FDT_TIMER=y
 CONFIG_FDT_TIMER_PLMT=y
 CONFIG_ANDES_TRIGGER=y
+CONFIG_ANDES_PMA=y

platform/generic/include/andes/andes45.h
@@ -1,18 +1,28 @@
 #ifndef _RISCV_ANDES45_H
 #define _RISCV_ANDES45_H
 
+/* clang-format off */
+
 #define CSR_MARCHID_MICROID 0xfff
 
 /* Memory and Miscellaneous Registers */
-#define CSR_MPFT_CTL 0x7c5
-#define CSR_MCACHE_CTL 0x7ca
+#define CSR_MPFT_CTL     0x7c5
+#define CSR_MCACHE_CTL   0x7ca
 #define CSR_MCCTLCOMMAND 0x7cc
 
 /* Supervisor Trap Related Registers */
 #define CSR_SLIP 0x9c5
 
-#define IRQ_M_PMU       18
+#define IRQ_M_PMU 18
 #define CSR_SLIP_PMOVI_MASK (1 << IRQ_M_PMU)
 #define CSR_MIE_PMOVI_MASK  (1 << IRQ_M_PMU)
+
+/* Configuration Control & Status Registers */
+#define CSR_MICM_CFG  0xfc0
+#define CSR_MDCM_CFG  0xfc1
+#define CSR_MMSC_CFG  0xfc2
+#define CSR_MMSC_CFG_PPMA_MASK (1 << 30)
+
+/* clang-format on */
 
 #endif /* _RISCV_ANDES45_H */

commit 180b999ab960cfe97ee5648686d4c64f563b3478

platform: generic: andes: Remove the error message in mcall_free_pma()
If a memory region wants to change its memory attribute from cacheable
to non-cacheable in Linux kernel, it needs to use ioremap_wc() to call
mcall_set_pma() to set PMA. And OpenSBI will record the VA in the PMA
mapping table. However, for a device region, since its default attribute
is non-cacheable, it usually does not need to set PMA to change its memory
attribute. Therefore, the kernel will call ioremap() but not mcall_set_pma().

Currently, both memory and device regions will call iounmap() in the kernel
and this function will eventually call mcall_free_pma(). When the driver
(device region) calls iounmap(), it will fail to find the corresponding VA
in the PMA mapping table.

It is more complicated to check whether to create PA in memory or device in
OpenSBI, as OpenSBI is not easy to convert from VA to PA. Therefore, we
ultimately decided to adopt a simpler method of removing the error message
to solve this problem.

Fixes: ed478347793e ("platform: generic: andes: Add PMA SBI call")
Signed-off-by: Dylan Jhong <dylan@andestech.com>

platform/generic/andes/pma.c
@@ -344,8 +344,5 @@ int mcall_free_pma(unsigned long va)
 		return 0;
 	}
 
-	sbi_printf("ERROR %s(): failed to free the PMA entry used by %#lx\n",
-		   __func__, va);
-
-	return SBI_ENOENT;
+	return 0;
 }

platform/generic/include/andes/pma.h
@@ -50,8 +50,7 @@ int mcall_set_pma(unsigned long pa, unsigned long va, unsigned long size);
  * Free a PMA entry
  * @param va virtual address, the associated PMA entry of the va will be freed
  *
- * @return 0 on success
- * @return SBI_ENOENT if entry not found
+ * @return 0 whether on success or fail
  */
 int mcall_free_pma(unsigned long va);
 