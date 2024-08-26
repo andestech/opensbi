andestech/opensbi

commit d5f1ca13c45847e6e1e203a50c0b532575c1197e

lib: utils: cache: Add L2C root domain region with R/W permissions for both M/S-mode.
In AST540, the cache is enabled in cleanup_before_linux() during U-boot
Normal Boot[1], before kernel starts. On AX65 CPU, however, when an SBI
ecall triggers OpenSBI to enable L2C, it results in a PMP load access
violation due to SMEPMP restrictions on M-mode permissions across the
entire address range. To prevent this, the L2C domain region and its
permissions must be pre-configured during OpenSBI Early Init.

List AX65 OpenSBI domain regions:
 Domain0 Region00:0x00000000f0300000-0x00000000f0300fff M:(I,R,W) S/U: (R,W)
 Domain0 Region01:0x00000000e0500000-0x00000000e050ffff M:(I,R,W) S/U: (R,W) <== L2C
 Domain0 Region02:0x0000000000040000-0x000000000005ffff M:(R,W) S/U: ()
 Domain0 Region03:0x0000000000000000-0x000000000003ffff M:(R,X) S/U: ()
 Domain0 Region04:0x00000000e6000000-0x00000000e60fffff M:(I,R,W) S/U: ()
 Domain0 Region05:0x00000000e6400000-0x00000000e67fffff M:(I,R,W) S/U: ()
 Domain0 Region06:0x00000000e4000000-0x00000000e5ffffff M:(I,R,W) S/U: (R,W)
 Domain0 Region07:0x0000000000000000-0xffffffffffffffff M: ()S/U: (R,W,X) <== SMEPMP

[1]: https://gitea.andestech.com/RD-SW/u-boot.official/commit/f0823a753718bd183f151df23095aa7e792890ae
Signed-off-by: Hui Min Mina Chou <minachou@andestech.com>

platform/generic/andes/ae350.c
@@ -114,8 +114,13 @@ static int ae350_final_init(bool cold_boot, const struct fdt_match *match)
 	pma_init();
 	ae350_hsm_device_init();
 	trigger_init();
-	fdt_cache_init();
 
+	return 0;
+}
+
+static int ae350_early_init(bool cold_boot, const struct fdt_match *match)
+{
+	fdt_cache_init();
 	return 0;
 }
 
@@ -126,6 +131,7 @@ static const struct fdt_match andes_ae350_match[] = {
 
 const struct platform_override andes_ae350 = {
 	.match_table = andes_ae350_match,
+	.early_init  = ae350_early_init,
 	.final_init  = ae350_final_init,
 	.extensions_init = andes_pmu_extensions_init,
 	.pmu_init = andes_pmu_init,

lib/utils/cache/fdt_cache_andes_l2c.c
@@ -64,6 +64,7 @@
 
 struct l2c_data {
 	unsigned long addr;
+	unsigned long size;
 	/* L2C Gen1/Gen2 quirks */
 	unsigned int cmd_stride;
 	unsigned int status_stride;
@@ -128,10 +129,10 @@ static struct cache andes_l2c = {
 static int andes_l2c_init(void *fdt, int nodeoff, const struct fdt_match *match)
 {
 	int rc;
-	uint64_t addr;
+	uint64_t addr, size;
 	ulong mmsc_cfg;
 
-	rc = fdt_get_node_addr_size(fdt, nodeoff, 0, &addr, NULL);
+	rc = fdt_get_node_addr_size(fdt, nodeoff, 0, &addr, &size);
 	if (rc)
 		return rc;
 
@@ -152,6 +153,7 @@ static int andes_l2c_init(void *fdt, int nodeoff, const struct fdt_match *match)
 	}
 
 	l2c.addr = (unsigned long)addr;
+	l2c.size = (unsigned long)size;
 	if (readl((void *)(l2c.addr + L2C_REG_CFG_OFFSET)) & MEM_MAP_MSK) {
 		/* v1 memory map (Gen2) */
 		l2c.cmd_stride	      = 0x1000;
@@ -163,6 +165,21 @@ static int andes_l2c_init(void *fdt, int nodeoff, const struct fdt_match *match)
 		l2c.status_stride     = 0x0;
 		l2c.status_bit_offset = 0x4;
 	}
+
+	/*
+	 * In AST540, the cache is enabled in cleanup_before_linux() during
+	 * U-boot Normal Boot, before kernel starts. On AX65 CPU, however,
+	 * when an SBI ecall triggers OpenSBI to enable L2C, it results in a
+	 * PMP load access violation due to SMEPMP restrictions on M-mode
+	 * permissions across the entire address range. To prevent this, the
+	 * L2C domain region and its permissions must be pre-configured during
+	 * OpenSBI platform early init.
+	 */
+	rc = sbi_domain_root_add_memrange(l2c.addr, l2c.size, 0x1000,
+		(SBI_DOMAIN_MEMREGION_MMIO |
+		 SBI_DOMAIN_MEMREGION_SHARED_SURW_MRW));
+	if (rc)
+		return rc;
 
 	rc = cache_add(&andes_l2c);
 	if (rc)

commit 6ec6f411a23762f85de4de3a92cb03b2709d4f5a

lib: utils: cache: L2C init is only needed at cold boot (#117)
L2C init is required only once during cold boot. Commit d5f1ca13c458
("lib: utils: cache: Add L2C root domain region with R/W permissions for
both M/S-mode.") caused it to run during warm boot, triggering the error:
"fdt_cache_driver_init: cache init failed, -3". This happens because,
after cold boot, the root domain region is protected [1]. Thus, when warm
boot tries to initialize L2C and attempts to add the root domain region
[2], it fails.

[1]: https://github.com/riscv-software-src/opensbi/blob/43cace6c3671e5172d0df0a8963e552bb04b7b20/lib/sbi/sbi_domain.c#L755
[2]: https://gitea.andestech.com/RD-SW/opensbi/src/commit/d5f1ca13c45847e6e1e203a50c0b532575c1197e/lib/utils/cache/fdt_cache_andes_l2c.c#L178

Fixes: d5f1ca13c458 ("lib: utils: cache: Add L2C root domain region with R/W permissions for both M/S-mode.")
Signed-off-by: Hui Min Mina Chou <minachou@andestech.com>
Reviewed-on: https://gitea.andestech.com/RD-SW/opensbi/pulls/117
Reviewed-by: Leo Yu-Chi Liang <ycliang@andestech.com>
Reviewed-by: Tim Shih-Ting OuYang <tim609@andestech.com>
Reviewed-by: Ben Zong-You Xie <ben717@andestech.com>
Reviewed-by: Peter Yu-Chien Lin <peterlin@andestech.com>
Reviewed-by: randolph <randolph@andestech.com>
Co-authored-by: Hui Min Mina Chou <minachou@andestech.com>
Co-committed-by: Hui Min Mina Chou <minachou@andestech.com>

platform/generic/andes/ae350.c
@@ -120,7 +120,9 @@ static int ae350_final_init(bool cold_boot, const struct fdt_match *match)
 
 static int ae350_early_init(bool cold_boot, const struct fdt_match *match)
 {
-	fdt_cache_init();
+	if (cold_boot)
+		fdt_cache_init();
+
 	return 0;
 }
 