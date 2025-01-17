andestech/opensbi

commit b80705b93d51af2c06bcde48deec8296370a1fa4

andes: fix Smepmp setting for ATCSMU mmio range (#123)
Makatau series CPU supports Smepmp extension, so we need to add
PMP setting for MMIO region that may be accessed in M-mode (ATCSMU).
However, we have already used up 8 PMP entries, which is also the maximum number
of PMP entry on AX45MP-8C bitmap and no more PMP entry we can use, therefore
we first check if the bitmap is Makatau serires CPU before using another PMP entry.

After this patch (on AX65)
Domain0 Region00 : 0x00000000f0100000-0x00000000f0100fff M: (I,R,W) S/U: (R,W)	// ATCSMU
Domain0 Region01 : 0x00000000f0300000-0x00000000f0300fff M: (I,R,W) S/U: (R,W)	// UART
Domain0 Region02 : 0x00000000e0500000-0x00000000e050ffff M: (I,R,W) S/U: (R,W)	// L2 cache
Domain0 Region03 : 0x0000000000000000-0x000000000003ffff M: (R,X) S/U: ()	// opensbi R/X region
Domain0 Region04 : 0x0000000000040000-0x000000000007ffff M: (R,W) S/U: ()	// opensbi R/W region
Domain0 Region05 : 0x00000000e6000000-0x00000000e60fffff M: (I,R,W) S/U: ()	// PLIC
Domain0 Region06 : 0x00000000e6400000-0x00000000e67fffff M: (I,R,W) S/U: ()	// PLIC-SW
Domain0 Region07 : 0x00000000e4000000-0x00000000e5ffffff M: (I,R,W) S/U: (R,W)	// PLMT
Domain0 Region08 : 0x0000000000000000-0xffffffffffffffff M: () S/U: (R,W,X)	// Else

After this patch (on AX45MP-8C)
Domain0 Region00 : 0x00000000f0300000-0x00000000f0300fff M: (I,R,W) S/U: (R,W)	// UART
Domain0 Region01 : 0x00000000e0500000-0x00000000e050ffff M: (I,R,W) S/U: (R,W)	// L2 cache
Domain0 Region02 : 0x0000000000000000-0x000000000003ffff M: (R,X) S/U: ()	// opensbi R/X region
Domain0 Region03 : 0x0000000000040000-0x000000000007ffff M: (R,W) S/U: ()	// opensbi R/W region
Domain0 Region04 : 0x00000000e6000000-0x00000000e60fffff M: (I,R,W) S/U: ()	// PLIC
Domain0 Region05 : 0x00000000e6400000-0x00000000e67fffff M: (I,R,W) S/U: ()	// PLIC-SW
Domain0 Region06 : 0x00000000e4000000-0x00000000e5ffffff M: (I,R,W) S/U: (R,W)	// PLMT

platform/generic/andes/ae350.c
@@ -333,6 +333,11 @@ static void ae350_smu_device_init(void)
 		sbi_hsm_set_device(&andes_smu_hsm);
 		sbi_system_suspend_set_device(&andes_smu_susp);
 
+		if ((csr_read(CSR_MARCHID) & 0xff) == 0x65)
+			rc = sbi_domain_root_add_memrange(smu.addr, 0x1000, 0x1000,
+							  SBI_DOMAIN_MEMREGION_MMIO |
+							  SBI_DOMAIN_MEMREGION_SHARED_SURW_MRW);
+
 		/* Allocate space for regs that need to be saved/restored */
 		save_regs_off = sbi_scratch_alloc_offset(sizeof(struct save_regs));
 		if (!save_regs_off)
@@ -346,7 +351,6 @@ static int ae350_final_init(bool cold_boot, const struct fdt_match *match)
 		return 0;
 
 	pma_init();
-	ae350_smu_device_init();
 	trigger_init();
 
 	return 0;
@@ -354,8 +358,10 @@ static int ae350_final_init(bool cold_boot, const struct fdt_match *match)
 
 static int ae350_early_init(bool cold_boot, const struct fdt_match *match)
 {
-	if (cold_boot)
+	if (cold_boot) {
+		ae350_smu_device_init();
 		return fdt_cache_init();
+	}
 
 	if (save_regs_off)
 		ae350_suspend_non_ret_restore(sbi_scratch_thishart_ptr(), false);

commit 1dfed2cf139883add44d33e3d4256bdbf74981f3

andes: ae350: fix Smepmp setting for ATCSMU mmio range (#131)
Previous commit uses "$marchid equals AX65" to determine whether
to add another PMP entry, but AX66 supports SMEPMP as well, so
use ISA string to check if SMEPMP is supported.

Fixes: b80705b93d51 ("andes: fix Smepmp setting for ATCSMU mmio range (#123)")
Signed-off-by: Leo Yu-Chi Liang <ycliang@andestech.com>

Reviewed-on: https://gitea.andestech.com/RD-SW/opensbi/pulls/131
Reviewed-by: Tim Shih-Ting OuYang <tim609@andestech.com>
Reviewed-by: Mina Hui-Min Chou <minachou@andestech.com>

platform/generic/andes/ae350.c
@@ -320,6 +320,19 @@ static struct sbi_system_suspend_device andes_smu_susp = {
 	.system_suspend		= ae350_system_suspend,
 };
 
+static inline bool ae350_support_smepmp(void)
+{
+	unsigned long extensions[BITS_TO_LONGS(SBI_HART_EXT_MAX)] = { 0 };
+
+	if (fdt_parse_isa_extensions(fdt_get_address(), current_hartid(), extensions))
+		return false;
+
+	if (__test_bit(SBI_HART_EXT_SMEPMP, extensions))
+		return true;
+
+	return false;
+}
+
 static void ae350_smu_device_init(void)
 {
 	int rc;
@@ -334,7 +347,14 @@ static void ae350_smu_device_init(void)
 		sbi_hsm_set_device(&andes_smu_hsm);
 		sbi_system_suspend_set_device(&andes_smu_susp);
 
-		if ((csr_read(CSR_MARCHID) & 0xff) == 0x65)
+		/** Quirk!
+		 * The PMP entry is not enough on some Kavalan bitmaps
+		 * (e.g., ax45mpv has only 8), so we use smepmp to
+		 * determine if we need to setup pmp region for SMU device.
+		 * Only Makatau ax65, ax66 bitmap support smepmp, and they
+		 * have 16 PMP entries.
+		 */
+		if (ae350_support_smepmp())
 			rc = sbi_domain_root_add_memrange(smu.addr, 0x1000, 0x1000,
 							  SBI_DOMAIN_MEMREGION_MMIO |
 							  SBI_DOMAIN_MEMREGION_SHARED_SURW_MRW);